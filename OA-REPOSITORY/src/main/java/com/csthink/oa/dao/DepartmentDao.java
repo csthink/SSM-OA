8cd9d0ac4fe14cbd7be69ed764865a663dea0b85
杜明洋 <security.2009@live.cn>
2019-04-03 15:59:50 +0800

    持久层接口方法添加注释

--- OA-REPOSITORY/src/main/java/com/csthink/oa/dao/DepartmentDao.java
+++ OA-REPOSITORY/src/main/java/com/csthink/oa/dao/DepartmentDao.java
@@ -8,13 +8,39 @@ import java.util.List;
 @Repository("departmentDao")
 public interface DepartmentDao {
 
-    int insert(Department department);
+    /**
+     * 保存部门信息
+     *
+     * @param department 部门对象
+     */
+    void insert(Department department);
 
-    int update(Department department);
+    /**
+     * 更新部门信息
+     *
+     * @param department 部门对象
+     */
+    void update(Department department);
 
-    int delete(Integer id);
+    /**
+     * 删除部门信息
+     *
+     * @param id 部门编号
+     */
+    void delete(Integer id);
 
+    /**
+     * 根据部门编号获取部门信息
+     *
+     * @param id 部门编号
+     * @return 部门对象
+     */
     Department selectById(Integer id);
 
+    /**
+     * 获取所有的部门集合
+     *
+     * @return 部门集合
+     */
     List<Department> selectAll();
 }
--- OA-REPOSITORY/src/main/java/com/csthink/oa/dao/EmployeeDao.java
+++ OA-REPOSITORY/src/main/java/com/csthink/oa/dao/EmployeeDao.java
@@ -8,13 +8,48 @@ import java.util.List;
 @Repository("employeeDao")
 public interface EmployeeDao {
 
-    int insert(Employee employee);
+    /**
+     * 保存员工信息
+     *
+     * @param employee 员工对象
+     */
+    void insert(Employee employee);
 
-    int update(Employee employee);
+    /**
+     * 更新员工信息
+     *
+     * @param employee 员工对象
+     */
+    void update(Employee employee);
 
-    int delete(Integer id);
+    /**
+     * 根据员工编号删除员工信息
+     *
+     * @param id 员工编号
+     */
+    void delete(Integer id);
 
+    /**
+     * 根据员工编号获取员工信息
+     *
+     * @param id 员工编号
+     * @return 员工对象
+     */
     Employee selectById(Integer id);
 
+    /**
+     * 获取全部的员工集合
+     *
+     * @return 员工集合
+     */
     List<Employee> selectAll();
+
+    /**
+     * 根据部门编号和职务查询出所有的员工
+     *
+     * @param deptId 部门编号
+     * @param post 职务
+     * @return 员工集合
+     */
+    List<Employee> selectByDeptIdAndPost(Integer deptId, String post);
 }
